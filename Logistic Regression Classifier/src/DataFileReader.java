4c46390e1a6481346eb99b7b81c9b7d31be84319
gopalmenon <gopalmenon@10.0.1.60>
2016-12-03 22:02:05 -0700

    First commit

---
+++ Logistic Regression Classifier/src/DataFileReader.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+/**
+ * Read train and test data files and parse data
+ *
+ */
+public class DataFileReader {
+	
+	private String trainingDataFileName, testingDataFileName;
+	private List<List<Double>> trainingDataFeatures, testingDataFeatures;
+	private List<BinaryDataLabel> trainingDataLabels, testingDataLabels;
+	
+
+}
